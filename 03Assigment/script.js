c4c3058e699085536f111394a0778c271e319724
fcotabar <fcotabar@gmail.com>
2020-12-31 13:23:20 -0500

    Third Assigment

---
+++ 03Assigment/script.js
@@ -0,0 +1,11 @@
+// Remember, we're gonna use strict mode in all scripts now!
+'use strict';
+
+const temperature = {
+    grades: 'celsius',
+    more: 'more2',
+    // number: Number(prompt('Some number please.')),
+    number: 10,
+};
+
+console.table(temperature);
